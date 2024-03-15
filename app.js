1a770f45e5e5f65e35dbcec17748275f7a44c689
felipekauan1 <152967390+felipekauan1@users.noreply.github.com>
2024-03-15 15:30:55 -0300

    v1.0

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    var classname = document.body.className;
+    if (classname == "light-theme") {
+        this.textContent = 'Dark';
+    } else {
+        this.textContent = 'Light';
+    }
+})
